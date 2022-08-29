78a1f167bfef734ae1d79e41f87398c33f49d43b
Fredrik Stetler <fredrikstetler@gmail.com>
2022-08-29 10:30:46 +0200

    Added readme

--- airport/src/main/java/Airport.java
+++ airport/src/main/java/Airport.java
@@ -1,5 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Airport {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("C:\\Programming\\Java\\AirportAccenture\\airport\\resources"));
+
         System.out.println("hey");
     }
 }
